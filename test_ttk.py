649f4dbce5f6adab02037cf5142c1cec83df55db
J. David Ibanez <jdavid@itaapy.com>
2009-03-23 18:16:42 +0100

    Use the most accurate version of the ttk tests.

--- test_ttk.py
+++ test_ttk.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright (C) 2008 Sylvain Taverne <sylvain@itaapy.com>
+# Copyright (C) 2008 David Versmisse <david.versmisse@itaapy.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,41 +14,39 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Import from the Standard Library
-import cStringIO
-import zipfile
-from string import whitespace
+# Import from itools
+from subprocess import Popen, PIPE
+
+# Import from itools
+from itools.xliff import XLFFile
+from itools.vfs import remove
+from itools.gettext.po import encode_source
 
 # Import from odf i18n tests
 from utils import start_test
 
-# Import from ttk
-from translate.storage.xml_extract import extract
-from translate.storage import odf_shared
-from translate.storage.xliff import xlifffile
 
+def ttk_odf2xliff_handler(filename):
+    # Make the xliff file
+    process = Popen(['odf2xliff', filename, 'tmp.xlf'],
+                    stdout=PIPE, stderr=PIPE)
+    if process.wait() != 0:
+        raise ValueError, '"%s" is malformed' % filename
 
-def ttk_handler(filename):
-    store = xlifffile()
+    # Get the units
+    units = []
+    xlf_file = XLFFile('tmp.xlf')
+    for a_file in xlf_file.files.values():
+        for unit in a_file.body.values():
+            units.append(encode_source(unit.source))
 
-    try:
-        z = zipfile.ZipFile(filename, 'r')
-        contents = z.read("content.xml")
-    except (ValueError, zipfile.BadZipfile):
-        contents = open(filename, 'r').read()
-    parse_state = extract.ParseState(odf_shared.no_translate_content_elements,
-                                     odf_shared.inline_elements)
-    extract.build_store(cStringIO.StringIO(contents), store, parse_state)
+    # Remove the tmp file
+    remove('tmp.xlf')
 
-    result = []
-    for unit in store.getunits():
-        unit = unit.getsource().strip(whitespace)
-        if unit.strip():
-            result.append(unit)
-
-    return result
+    return units
 
 
 if __name__ == '__main__':
-    start_test(ttk_handler)
+    start_test(ttk_odf2xliff_handler)
+
 
